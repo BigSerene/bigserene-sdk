8051ad30c7a36ceadeef0ebaeca815a830fcabff
Chris Lee <sihrc.c.lee@gmail.com>
2020-05-25 15:24:37 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     description="Bigserene SDK to access Bigserene App Platform",
     author="Chris Lee",
     author_email="chrisl@bigserene.com",
-    packages=find_packages(),
+    packages=find_packages() + ["bscli"],
     install_requires=["click==7.0", "pytz==2020.1", "requests==2.21.0", "tqdm==4.45.0"],
     extras_require={"test": ["pytest==4.0.0"]},
     scripts=["bin/bscli"],
